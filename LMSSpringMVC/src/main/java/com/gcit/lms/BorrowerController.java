4cfa3b7d23b77a72adcb7146551ac87ec9f3f586
ppradhan <ppradhan@ppradhan3510.VENTERA.com>
2019-06-25 19:19:50 -0400

    Initial push with MVC Code only.

---
+++ LMSSpringMVC/src/main/java/com/gcit/lms/BorrowerController.java
@@ -0,0 +1,26 @@
+package com.gcit.lms;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class BorrowerController {
+	
+	@RequestMapping(value = "/borrower", method = RequestMethod.GET)
+	public String lmsHome(Locale locale, Model model) {
+		return "lmsborrower";
+	}
+	
+	
+}
---
+++ LMSSpringMVC/src/main/webapp/WEB-INF/views/lmsborrower.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>LMS Home</title>
+</head>
+<body>
+<h1>
+	This is LMS Borrower's Home Page
+</h1>
+
+<P>  
+</body>
+</html>
